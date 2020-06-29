5abb260ab8c1617082d80917a4d64b07008bba8e
Avisrara74 <avisrara74@gmail.com>
2020-06-29 17:34:30 +0500

    Updates

---
+++ precache-manifest.98fac2c262f49a3a2c58854eadeca48d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee8520f4436128c3de916267a2ae6918",
+    "url": "/jm_redux-auth-reg/index.html"
+  },
+  {
+    "revision": "70220e9bda889cccbb96",
+    "url": "/jm_redux-auth-reg/static/css/2.3cb76441.chunk.css"
+  },
+  {
+    "revision": "4db438383cac983d6145",
+    "url": "/jm_redux-auth-reg/static/css/main.61eec482.chunk.css"
+  },
+  {
+    "revision": "70220e9bda889cccbb96",
+    "url": "/jm_redux-auth-reg/static/js/2.4fbd276b.chunk.js"
+  },
+  {
+    "revision": "4f160e29840e1f64ec2468f8c9029bf3",
+    "url": "/jm_redux-auth-reg/static/js/2.4fbd276b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4db438383cac983d6145",
+    "url": "/jm_redux-auth-reg/static/js/main.54ab57fb.chunk.js"
+  },
+  {
+    "revision": "c1bd4e477521385d24d1",
+    "url": "/jm_redux-auth-reg/static/js/runtime-main.ae55e092.js"
+  }
+]);
